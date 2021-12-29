5c8932b0cc25cbe257a5e26b850ca3b38ce6e5b0
Pierre <pierre@delange.design>
2021-12-28 17:42:49 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace RelRotateTest
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			robotImg.AnchorX = 0.5;
+			robotImg.AnchorY = 0.5;
+			robotImg.RelRotateTo(45, 250, Easing.CubicOut);
+		}
+	}
+}
